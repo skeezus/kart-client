fda6021f3d42e2aa0fad9cafc2eefd3a7ef3e974
Braden Holt <bradenholt@gmail.com>
2020-07-15 08:49:15 -0600

    -editor progress

--- src/containers/playground/playground.js
+++ src/containers/playground/playground.js
@@ -5,15 +5,19 @@ import './playground.css'
 
 const PlaygroundContainer = (props) => {
   
-  useEffect(() => { // Similar to componentDidMount and componentDidUpdate:
-    document.addEventListener("keydown", function(e) {
-      if(e.keyCode === 9) { // 9 is tab key
-        //document.execCommand('insertHTML', false, '&#009'); //add tab
-        //document.execCommand('indent', false, null)
-        const TAB_SIZE = 3
-        document.execCommand('insertText', false, ' '.repeat(TAB_SIZE));
+    useEffect(() => { // Similar to componentDidMount and componentDidUpdate:
+        document.addEventListener("keydown", function(e) {
+            if(e.keyCode === 8) { // 8 is delete key
+                //e.preventDefault() // prevent default if close to deleting number
+            } else if(e.keyCode === 9) { // 9 is tab key
+                //document.execCommand('insertHTML', false, '&#009'); //add tab
+                //document.execCommand('indent', false, null)
+                const TAB_SIZE = 3
+                document.execCommand('insertText', false, ' '.repeat(TAB_SIZE));
 
-        e.preventDefault() // prevent focusing on next element (the search box in some cases)
+                e.preventDefault() // prevent focusing on next element (the search box in some cases)
+            } else if(e.keyCode === 13) {
+                // add new p row
       }
     })
   });
@@ -21,15 +25,19 @@ const PlaygroundContainer = (props) => {
     return(
         <>
           <div className="container">
-            <pre>
-              <div className="editor" contentEditable="true">
-                  {
-                    Array(20).fill().map((_,i) => (
-                      <p className="rowNumber">{i+1}</p>
-                    ))
-                  }
-              </div>
-            </pre>
+            <div className="editor">
+                <pre>
+                    <div className="lineNumbers">
+                        {
+                            Array(20).fill().map((_,i) => (
+                            <p className="rowNumber">{i+1}</p>
+                            ))
+                        }
+                    </div>
+                    <div className="content" contentEditable="true">
+                    </div>
+                </pre>
+            </div>
           </div>
         </>
     );
